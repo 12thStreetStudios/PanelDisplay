619b498032d348c737692d2f776b23fd29680418
Russell <rdaswimmer@gmail.com>
2020-12-03 17:55:00 -0500

    created webpage.

--- panel-web-app/src/App.js
+++ panel-web-app/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+const { exec } = require("child_process")
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class MessageForm extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value})
+  }
+
+  handleSubmit(event) {
+    exec(this.state.value + " | pwrite")
+    event.preventDefault();
+  }
+
+  render() {
+    return (
+      <div class="Message">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" value={this.state.value} onChange={this.handleChange} />
+          <input type="submit" value="Send" />
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
